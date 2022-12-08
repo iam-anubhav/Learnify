2ae905561a71062236c046d16dfa348149287309
Anubhav <anubhavv909@gmail.com>
2022-12-08 17:44:39 +0530

    Udated signup.html and views.py. Added the validations in the signup form and also added Users app to store the user data un the database.

--- Learnify/Learnify/views.py
+++ Learnify/Learnify/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from Users.models import UserData
+from django.contrib import messages
 
 def index(request):
     return render(request,'index.html')
@@ -8,6 +10,28 @@ def signin(request):
     return render(request,'signin.html')
 
 def signup(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm = request.POST['confirm']
+
+        details = [name,email,password]
+        if '' in details:
+            messages.success(request, "Some field is empty")
+            return redirect('/signup')
+        elif '@' not in email:
+            messages.success(request, "Invalid Email")
+            return redirect('/signup')
+        elif password != confirm:
+            messages.success(request,"Password do not matched")
+            return redirect('/signup')
+        else:
+            data = UserData(name=name,email=email, password=password)
+            data.save()
+            messages.success(request, "You are registered. Sign in now")
+            return redirect('/signup')
+
     return render(request,'signup.html')
 
 def aboutus(request):
